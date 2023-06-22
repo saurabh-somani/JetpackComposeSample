76e30a39b7c332514a73894114f0c4a9ed5b009c
Saurabh Somani <50082769+saurabh-somani@users.noreply.github.com>
2023-06-23 02:16:58 +0530

    Persisting state (rememberSaveable).

--- app/src/main/java/com/example/jetpackcomposesample/ui/screens/MyApp.kt
+++ app/src/main/java/com/example/jetpackcomposesample/ui/screens/MyApp.kt
@@ -4,13 +4,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier) {
         if (shouldShowOnboarding) {
