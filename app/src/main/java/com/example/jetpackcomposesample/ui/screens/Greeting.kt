b76dc2a61b571bc4ebab1679c52123c7d4f1d670
Saurabh Somani <50082769+saurabh-somani@users.noreply.github.com>
2023-06-25 00:50:06 +0530

    Theming.

--- app/src/main/java/com/example/jetpackcomposesample/ui/screens/Greeting.kt
+++ app/src/main/java/com/example/jetpackcomposesample/ui/screens/Greeting.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackcomposesample.ui.screens
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -18,8 +19,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 
 @Composable
 fun Greetings() {
@@ -53,12 +57,30 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             
             ElevatedButton(onClick = { expanded = !expanded }) {
                Text(if (expanded) "Show less" else "Show more")
             }
         }
+    }
+}
+
+@Preview(name = "Light")
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Composable
+fun GreetingsPreview() {
+    JetpackComposeSampleTheme {
+        Greetings()
     }
 }
